5e0d0a6f7b21299b0b41dc4e324cffd6d9dcfecc
Ayush <ayushnvijay@users.noreply.github.com>
2018-01-15 22:16:32 -0500

    Update ImgurAuth.java

--- app/src/main/java/com/example/ayush/androidchallenge/ImgurAuth.java
+++ app/src/main/java/com/example/ayush/androidchallenge/ImgurAuth.java
@@ -1,5 +1,5 @@
 package com.example.ayush.androidchallenge;
 
 public final class ImgurAuth {
-    public static final String CLIENT_ID = "Authorization: Client-ID 94a876b2db8e977";
+    public static final String CLIENT_ID = "Authorization: Client-ID Your Client Id";
 }
